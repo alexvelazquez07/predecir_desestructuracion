030032c81e614f8dbb616d1c9736295950a6f9ae
Alex Velazquez <arichard.1190@gmail.com>
2022-12-17 21:02:10 -0300

    Predecir desestructuracion

---
+++ predecir_desestructuracion.js
@@ -0,0 +1,58 @@
+/* 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+// Imprimira Tesla, Mercerdes
+console.log(randomCar)
+console.log(otherRandomCar)*/
+
+/* 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Generará error: name is not defined
+console.log(name);
+console.log(otherName);*/
+
+/* 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Imprimira : 12345, undefined
+console.log(password);
+console.log(hashedPassword);*/
+
+
+/* 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Imprimira : false, true
+console.log(first == second);
+console.log(first == third); */
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Imprimira : value [1,5,1,8,3,3] 1 5
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+
+
+
+
